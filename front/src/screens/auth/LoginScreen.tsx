5b8241c72ce0f528991c35fe627b418d9ea03da1
InKyoJeong <viapolar@naver.com>
2023-11-18 19:14:56 +0900

    feat(3-2): add login screen inputs

--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,14 +1,77 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import InputField from '../../components/InputField';
 
 function LoginScreen() {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+  });
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+
+  // const handleChangeEmail = (text: string) => {
+  //   setEmail(text);
+  // };
+
+  // const handleChangePassword = (text: string) => {
+  //   setPassword(text);
+  // };
+
+  const handleChangeText = (name: string, text: string) => {
+    setValues({
+      ...values,
+      [name]: text,
+    });
+  };
+
+  const handleBlur = (name: string) => {
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
+
   return (
-    <View>
-      <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          autoFocus
+          value={values.email}
+          onChangeText={text => handleChangeText('email', text)}
+          onBlur={() => handleBlur('email')}
+          inputMode="email"
+          returnKeyType="next"
+          placeholder="이메일"
+          error={'이메일 에러!'}
+        />
+        <InputField
+          autoFocus
+          value={values.password}
+          onChangeText={text => handleChangeText('password', text)}
+          onBlur={() => handleBlur('password')}
+          returnKeyType="next"
+          placeholder="비밀번호"
+          error={'비밀번호 에러!'}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 30,
+    marginBottom: 40,
+  },
+});
 
 export default LoginScreen;
