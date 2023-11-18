4e3c2fa50c79161efc88adb117f65a6a0c3a86fb
InKyoJeong <viapolar@naver.com>
2023-11-18 19:13:24 +0900

    feat(3-2): add inputField component

---
+++ front/src/components/InputField.tsx
@@ -0,0 +1,90 @@
+import React, {useRef} from 'react';
+import {
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextInputProps,
+  View,
+} from 'react-native';
+import {colors} from '../constants';
+
+interface InputFieldProps extends TextInputProps {
+  error?: string;
+  disabled?: boolean;
+}
+
+const deviceHeight = Dimensions.get('screen').height;
+
+function InputField({error, disabled = false, ...props}: InputFieldProps) {
+  const innerRef = useRef<TextInput | null>(null);
+
+  const handlePressInput = () => {
+    innerRef.current?.focus();
+  };
+
+  return (
+    <Pressable onPress={handlePressInput}>
+      <View
+        style={[
+          styles.container,
+          props.multiline && styles.multiLine,
+          disabled && styles.disabled,
+          Boolean(error) && styles.inputError,
+        ]}>
+        <View style={styles.innerContainer}>
+          <TextInput
+            style={[styles.input, disabled && styles.disabled]}
+            autoCapitalize="none"
+            placeholderTextColor={colors.GRAY_500}
+            editable={!disabled}
+            autoCorrect={false}
+            spellCheck={false}
+            {...props}
+          />
+        </View>
+        {Boolean(error) && <Text style={styles.error}>{error}</Text>}
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: colors.GRAY_200,
+    paddingVertical: deviceHeight > 640 ? 15 : 10,
+    paddingHorizontal: deviceHeight > 640 ? 15 : 10,
+  },
+  multiLine: {
+    paddingBottom: deviceHeight > 640 ? 45 : 30,
+  },
+  input: {
+    fontSize: 16,
+    paddingVertical: 0,
+    paddingLeft: 0,
+    color: colors.BLACK,
+  },
+  innerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5,
+  },
+  disabled: {
+    backgroundColor: colors.GRAY_200,
+    color: colors.GRAY_700,
+  },
+  inputError: {
+    borderWidth: 1,
+    borderColor: colors.RED_300,
+  },
+  error: {
+    color: colors.RED_500,
+    fontSize: 12,
+    paddingTop: 5,
+    paddingLeft: 0,
+  },
+});
+
+export default InputField;
--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -1,7 +1,14 @@
 const colors = {
   PINK_500: '#BF5C79',
   PINK_700: '#C63B64',
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
   WHITE: '#FFF',
+  GRAY_100: '#F8F8F8',
+  GRAY_200: '#E7E7E7',
+  GRAY_300: '#D8D8D8',
+  GRAY_500: '#8E8E8E',
+  GRAY_700: '#575757',
   BLACK: '#000',
 };
 
