65770c00705888f7f6cbcade9780d030e00bfe7b
InKyoJeong <viapolar@naver.com>
2023-11-26 22:28:00 +0900

    feat(3-8): add query provider & queryClient

--- front/App.tsx
+++ front/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import {QueryClientProvider} from '@tanstack/react-query';
+
 import RootNavigator from './src/navigations/root/RootNavigator';
+import queryClient from './src/api/queryClient';
 
 function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
---
+++ front/src/api/queryClient.ts
@@ -0,0 +1,14 @@
+import {QueryClient} from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+});
+
+export default queryClient;
