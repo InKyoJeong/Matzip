9c931b13e9da3d0be54c125185894df3e67b84c6
InKyoJeong <viapolar@naver.com>
2023-11-27 23:39:02 +0900

    feat(3-9): add logout button

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
+
+import useAuth from '@/hooks/queries/useAuth';
 
 function MapHomeScreen() {
+  const {logoutMutation} = useAuth();
+
+  const handleLogout = () => {
+    logoutMutation.mutate(null);
+  };
+
   return (
     <SafeAreaView>
       <Text>맵 스크린</Text>
+      <Button title="로그아웃" onPress={handleLogout} />
     </SafeAreaView>
   );
 }
