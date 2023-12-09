9d2663425bfbded1c3a2ce6b1b113081fd1c81f9
InKyoJeong <viapolar@naver.com>
2023-12-10 01:47:25 +0900

    feat(4-1): move map to user location

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {Pressable, StyleSheet, Text} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, LatLng} from 'react-native-maps';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
@@ -18,19 +18,34 @@ type Navigation = CompositeNavigationProp<
 >;
 
 function MapHomeScreen() {
+  const mapRef = useRef<MapView | null>(null);
   const {logoutMutation} = useAuth();
   const insets = useSafeAreaInsets();
   const navigation = useNavigation<Navigation>();
 
-  const [userLocation, setUserLocation] = useState<LatLng>();
+  const [userLocation, setUserLocation] = useState<LatLng>({
+    latitude: 37.5516032365118,
+    longitude: 126.98989626020192,
+  });
   const [isUserLocationError, setIsUserLocationError] = useState(false);
-
-  console.log('userLocation', userLocation);
-  //1: 나의 위치 구하기
-  //2: 지도를 이동시키기
 
   const handleLogout = () => {
     logoutMutation.mutate(null);
+  };
+
+  const handlePressUserLocation = () => {
+    console.log('userLocation', userLocation);
+    if (isUserLocationError) {
+      // 에러메세지 표시하기
+      return;
+    }
+
+    mapRef.current?.animateToRegion({
+      latitude: userLocation.latitude,
+      longitude: userLocation.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
   };
 
   useEffect(() => {
@@ -52,17 +67,23 @@ function MapHomeScreen() {
   return (
     <>
       <MapView
+        ref={mapRef}
         style={styles.container}
         provider={PROVIDER_GOOGLE}
         showsUserLocation
         followsUserLocation
-        showsMyLocationButton={true}
+        showsMyLocationButton={false}
       />
       <Pressable
         style={[styles.drawerButton, {top: insets.top || 20}]}
         onPress={() => navigation.openDrawer()}>
         <Text>서랍</Text>
       </Pressable>
+      <View style={styles.buttonList}>
+        <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+          <Text>내위치</Text>
+        </Pressable>
+      </View>
     </>
   );
 }
@@ -83,6 +104,24 @@ const styles = StyleSheet.create({
     shadowOffset: {width: 1, height: 1},
     shadowOpacity: 0.5,
     elevation: 4,
+  },
+  buttonList: {
+    position: 'absolute',
+    bottom: 50,
+    right: 15,
+  },
+  mapButton: {
+    marginVertical: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 48,
+    width: 48,
+    backgroundColor: colors.PINK_700,
+    borderRadius: 30,
+    shadowColor: colors.BLACK,
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.5,
+    elevation: 2,
   },
 });
 
