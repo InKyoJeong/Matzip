542c241bb2ee0aac99c311acf668210f19d99ac2
InKyoJeong <viapolar@naver.com>
2023-12-09 21:34:56 +0900

    chore(4-0): install react native maps

--- front/ios/MatzipApp/AppDelegate.mm
+++ front/ios/MatzipApp/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyD30G2DIRx_c73iPAegAi7OWSiV1jdZzkw"];
   self.moduleName = @"MatzipApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
