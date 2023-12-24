b8fd31306e0b39e6165e99c5fb08eb022b5b7e95
InKyoJeong <viapolar@naver.com>
2023-12-25 03:47:45 +0900

    feat(5-2): get route params

--- front/src/screens/map/AddPostScreen.tsx
+++ front/src/screens/map/AddPostScreen.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {StackScreenProps} from '@react-navigation/stack';
 
-interface AddPostScreenProps {}
+import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
+import {mapNavigations} from '@/constants';
 
-function AddPostScreen({}: AddPostScreenProps) {
-  return <View></View>;
+type AddPostScreenProps = StackScreenProps<
+  MapStackParamList,
+  typeof mapNavigations.ADD_POST
+>;
+
+function AddPostScreen({route}: AddPostScreenProps) {
+  const {location} = route.params;
+
+  return (
+    <View>
+      <Text>{location.latitude}</Text>
+      <Text>{location.longitude}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
