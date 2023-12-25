485c13bac6c10fdbdc675bb42f863c8dd24d8e83
InKyoJeong <viapolar@naver.com>
2023-12-25 23:32:48 +0900

    feat(5-3): invalidate and setQueryData

--- front/src/hooks/queries/useMutateCreatePost.ts
+++ front/src/hooks/queries/useMutateCreatePost.ts
@@ -1,11 +1,36 @@
 import {useMutation} from '@tanstack/react-query';
 
 import {createPost} from '@/api/post';
-import {UseMutationCustomOptions} from '@/types';
+import {Marker, UseMutationCustomOptions} from '@/types';
+import queryClient from '@/api/queryClient';
+import {queryKeys} from '@/constants';
 
 function useMutateCreatePost(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: createPost,
+    onSuccess: newPost => {
+      // queryClient.invalidateQueries({
+      //   queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+      // });
+
+      queryClient.setQueryData<Marker[]>(
+        [queryKeys.MARKER, queryKeys.GET_MARKERS],
+        existingMarkers => {
+          const newMarker = {
+            id: newPost.id,
+            latitude: newPost.latitude,
+            longitude: newPost.longitude,
+            color: newPost.color,
+            score: newPost.score,
+          };
+
+          return existingMarkers
+            ? [...existingMarkers, newMarker]
+            : [newMarker];
+        },
+      );
+    },
+
     ...mutationOptions,
   });
 }
