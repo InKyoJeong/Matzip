f5b4d7e553a92b721269cb26e07ea9dcb132512e
InKyoJeong <viapolar@naver.com>
2024-01-28 02:53:36 +0900

    feat(6-5): add image carousel

---
+++ front/src/components/common/ImageCarousel.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+  Dimensions,
+  StyleSheet,
+  View,
+  FlatList,
+  Pressable,
+  Platform,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Octicons from 'react-native-vector-icons/Octicons';
+
+import {colors} from '@/constants';
+import type {ImageUri} from '@/types';
+import {Image} from 'react-native';
+
+interface ImageCarouselProps {
+  images: ImageUri[];
+}
+
+const deviceWidth = Dimensions.get('window').width;
+
+function ImageCarousel({images}: ImageCarouselProps) {
+  const insets = useSafeAreaInsets();
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Pressable
+        style={[styles.backButton, {marginTop: insets.top + 10}]}
+        onPress={() => navigation.goBack()}>
+        <Octicons name="arrow-left" size={30} color={colors.WHITE} />
+      </Pressable>
+
+      <FlatList
+        data={images}
+        renderItem={({item}) => (
+          <View style={{width: deviceWidth}}>
+            <Image
+              style={styles.image}
+              source={{
+                uri: `${
+                  Platform.OS === 'ios'
+                    ? 'http://localhost:3030/'
+                    : 'http://10.0.2.2:3030/'
+                }${item.uri}`,
+              }}
+              resizeMode="contain"
+            />
+          </View>
+        )}
+        keyExtractor={item => String(item.id)}
+        horizontal
+        pagingEnabled
+        decelerationRate="fast"
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: colors.WHITE,
+  },
+  backButton: {
+    position: 'absolute',
+    left: 20,
+    zIndex: 1,
+    backgroundColor: colors.PINK_700,
+    height: 40,
+    width: 40,
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    width: 200,
+    height: 200,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default ImageCarousel;
