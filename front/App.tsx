3a4a58708082b40281a2da0215c5c146e293f03f
InKyoJeong <viapolar@naver.com>
2024-02-11 02:23:16 +0900

    feat(8-0): add custom toast style

--- front/App.tsx
+++ front/App.tsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {QueryClientProvider} from '@tanstack/react-query';
-import Toast from 'react-native-toast-message';
+import Toast, {
+  BaseToast,
+  BaseToastProps,
+  ErrorToast,
+} from 'react-native-toast-message';
 
 import RootNavigator from './src/navigations/root/RootNavigator';
 import queryClient from './src/api/queryClient';
+import {colors} from '@/constants';
+
+const toastConfig = {
+  success: (props: BaseToastProps) => (
+    <BaseToast
+      {...props}
+      style={{borderLeftColor: colors.BLUE_500}}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+  error: (props: BaseToastProps) => (
+    <ErrorToast
+      {...props}
+      style={{borderLeftColor: colors.RED_500}}
+      text1Style={{
+        fontSize: 14,
+      }}
+      text2Style={{
+        fontSize: 12,
+      }}
+    />
+  ),
+};
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
         <RootNavigator />
-        <Toast />
+        <Toast config={toastConfig} />
       </NavigationContainer>
     </QueryClientProvider>
   );
