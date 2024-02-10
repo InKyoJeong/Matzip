871154e8d457d08e8ed2ad9dfe0cbc0f51ec09bf
InKyoJeong <viapolar@naver.com>
2024-02-11 02:16:52 +0900

    chore(8-0): install toast message

--- front/App.tsx
+++ front/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {QueryClientProvider} from '@tanstack/react-query';
+import Toast from 'react-native-toast-message';
 
 import RootNavigator from './src/navigations/root/RootNavigator';
 import queryClient from './src/api/queryClient';
@@ -10,6 +11,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
         <RootNavigator />
+        <Toast />
       </NavigationContainer>
     </QueryClientProvider>
   );
