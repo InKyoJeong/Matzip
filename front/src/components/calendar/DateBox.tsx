bb33a4c19b82eda41fe6ec0b8944fd7bac3332c9
InKyoJeong <viapolar@naver.com>
2024-02-18 20:41:02 +0900

    feat(8-5): apply darkMode

--- front/src/components/calendar/DateBox.tsx
+++ front/src/components/calendar/DateBox.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Dimensions, Pressable, StyleSheet, Text, View} from 'react-native';
 
 import {colors} from '@/constants';
+import useThemeStore from '@/store/useThemeStore';
+import {ThemeMode} from '@/types';
 
 interface DateBoxProps {
   date: number;
@@ -20,6 +22,9 @@ function DateBox({
   selectedDate,
   onPressDate,
 }: DateBoxProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
   return (
     <Pressable style={styles.container} onPress={() => onPressDate(date)}>
       {date > 0 && (
@@ -47,50 +52,51 @@ function DateBox({
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    width: deviceWidth / 7,
-    height: deviceWidth / 7,
-    borderTopWidth: StyleSheet.hairlineWidth,
-    borderTopColor: colors.GRAY_200,
-    alignItems: 'center',
-  },
-  dateContainer: {
-    marginTop: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 28,
-    height: 28,
-    borderRadius: 28,
-  },
-  selectedContainer: {
-    backgroundColor: colors.BLACK,
-  },
-  selectedTodayContainer: {
-    backgroundColor: colors.PINK_700,
-  },
-  dateText: {
-    fontSize: 17,
-    color: colors.BLACK,
-  },
-  todayText: {
-    color: colors.PINK_700,
-    fontWeight: 'bold',
-  },
-  selectedDateText: {
-    color: colors.WHITE,
-    fontWeight: 'bold',
-  },
-  selectedTodayText: {
-    color: colors.WHITE,
-  },
-  scheduleIndicator: {
-    marginTop: 2,
-    width: 6,
-    height: 6,
-    borderRadius: 6,
-    backgroundColor: colors.GRAY_500,
-  },
-});
+const styling = (theme: ThemeMode) =>
+  StyleSheet.create({
+    container: {
+      width: deviceWidth / 7,
+      height: deviceWidth / 7,
+      borderTopWidth: StyleSheet.hairlineWidth,
+      borderTopColor: colors[theme].GRAY_200,
+      alignItems: 'center',
+    },
+    dateContainer: {
+      marginTop: 5,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 28,
+      height: 28,
+      borderRadius: 28,
+    },
+    selectedContainer: {
+      backgroundColor: colors[theme].BLACK,
+    },
+    selectedTodayContainer: {
+      backgroundColor: colors[theme].PINK_700,
+    },
+    dateText: {
+      fontSize: 17,
+      color: colors[theme].BLACK,
+    },
+    todayText: {
+      color: colors[theme].PINK_700,
+      fontWeight: 'bold',
+    },
+    selectedDateText: {
+      color: colors[theme].WHITE,
+      fontWeight: 'bold',
+    },
+    selectedTodayText: {
+      color: colors[theme].WHITE,
+    },
+    scheduleIndicator: {
+      marginTop: 2,
+      width: 6,
+      height: 6,
+      borderRadius: 6,
+      backgroundColor: colors[theme].GRAY_500,
+    },
+  });
 
 export default DateBox;
