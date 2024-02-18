924f2dc6adc3ebc0718dd9915fd7529d12605d0f
InKyoJeong <viapolar@naver.com>
2024-02-18 19:36:49 +0900

    feat(8-5): add color constants for darkMode

--- front/src/constants/colors.ts
+++ front/src/constants/colors.ts
@@ -1,31 +1,49 @@
-const colors = {
+const common = {
   PINK_200: '#FAE2E9',
   PINK_400: '#EC87A5',
   PINK_500: '#BF5C79',
   PINK_700: '#C63B64',
-  RED_300: '#FFB4B4',
-  RED_500: '#FF5F5F',
-  BLUE_400: '#B4E0FF',
   BLUE_500: '#0D8AFF',
+  RED_500: '#FF5F5F',
+  RED_300: '#FFB4B4',
+  BLUE_400: '#B4E0FF',
   GREEN_400: '#CCE6BA',
   YELLOW_400: '#FFE594',
   YELLOW_500: '#FACC15',
   PURPLE_400: '#C4C4E7',
-  WHITE: '#FFF',
-  GRAY_100: '#F8F8F8',
-  GRAY_200: '#E7E7E7',
-  GRAY_300: '#D8D8D8',
-  GRAY_500: '#8E8E8E',
-  GRAY_700: '#575757',
-  BLACK: '#000',
+  UNCHANGE_WHITE: '#FFF',
+  UNCHANGE_BLACK: '#000',
 };
 
+const colors = {
+  light: {
+    WHITE: '#FFF',
+    GRAY_100: '#F8F8F8',
+    GRAY_200: '#E7E7E7',
+    GRAY_300: '#D8D8D8',
+    GRAY_500: '#8E8E8E',
+    GRAY_700: '#575757',
+    BLACK: '#161616',
+    ...common,
+  },
+  dark: {
+    WHITE: '#161616',
+    GRAY_100: '#202124',
+    GRAY_200: '#3C4043',
+    GRAY_300: '#5e5e5e',
+    GRAY_500: '#8E8E8E',
+    GRAY_700: '#F8F8F8',
+    BLACK: '#fff',
+    ...common,
+  },
+} as const;
+
 const colorHex = {
-  RED: colors.PINK_400,
-  BLUE: colors.BLUE_400,
-  GREEN: colors.GREEN_400,
-  YELLOW: colors.YELLOW_400,
-  PURPLE: colors.PURPLE_400,
+  RED: colors['light'].PINK_400,
+  BLUE: colors['light'].BLUE_400,
+  GREEN: colors['light'].GREEN_400,
+  YELLOW: colors['light'].YELLOW_400,
+  PURPLE: colors['light'].PURPLE_400,
 } as const;
 
 export {colors, colorHex};
