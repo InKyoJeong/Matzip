563262a2d18f87ec9210b47a3d747c7701dfdec4
InKyoJeong <viapolar@naver.com>
2024-02-18 21:46:46 +0900

    feat(8-6): add compoundOption sub component

--- front/src/components/common/CompoundOption.tsx
+++ front/src/components/common/CompoundOption.tsx
@@ -11,6 +11,7 @@ import {
   View,
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import {colors} from '@/constants';
 import useThemeStore from '@/store/useThemeStore';
@@ -128,12 +129,71 @@ function Divider() {
   return <View style={styles.border} />;
 }
 
+interface CheckBoxProps extends PressableProps {
+  children: ReactNode;
+  icon?: ReactNode;
+  isChecked?: boolean;
+}
+
+function CheckBox({
+  children,
+  icon,
+  isChecked = false,
+  ...props
+}: CheckBoxProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  return (
+    <Pressable
+      style={({pressed}) => [
+        pressed && styles.optionButtonPressed,
+        styles.checkBoxContainer,
+      ]}
+      {...props}>
+      <Ionicons
+        name={`checkmark-circle${isChecked ? '' : '-outline'}`}
+        size={22}
+        color={colors[theme].BLUE_500}
+      />
+      {icon}
+      <Text style={styles.checkBoxText}>{children}</Text>
+    </Pressable>
+  );
+}
+
+interface FilterProps extends PressableProps {
+  children: ReactNode;
+  isSelected?: boolean;
+}
+
+function Filter({children, isSelected, ...props}: FilterProps) {
+  const {theme} = useThemeStore();
+  const styles = styling(theme);
+
+  return (
+    <Pressable style={styles.filterContainer} {...props}>
+      <Text
+        style={[isSelected ? styles.filterSelectedText : styles.filterText]}>
+        {children}
+      </Text>
+      <MaterialIcons
+        name="keyboard-arrow-down"
+        size={22}
+        color={isSelected ? colors[theme].BLUE_500 : colors[theme].GRAY_300}
+      />
+    </Pressable>
+  );
+}
+
 export const CompoundOption = Object.assign(OptionMain, {
   Container,
   Background,
   Button,
   Title,
   Divider,
+  CheckBox,
+  Filter,
 });
 
 const styling = (theme: ThemeMode) =>
@@ -180,5 +240,32 @@ const styling = (theme: ThemeMode) =>
     border: {
       borderBottomColor: colors[theme].GRAY_200,
       borderBottomWidth: 1,
+    },
+    checkBoxContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingVertical: 10,
+      paddingHorizontal: 30,
+      gap: 10,
+    },
+    checkBoxText: {
+      color: colors[theme].BLACK,
+      fontSize: 15,
+    },
+    filterContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+      gap: 5,
+    },
+    filterText: {
+      color: colors[theme].GRAY_300,
+      fontSize: 15,
+      fontWeight: '500',
+    },
+    filterSelectedText: {
+      color: colors[theme].BLUE_500,
+      fontSize: 15,
+      fontWeight: '500',
     },
   });
