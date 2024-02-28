73b3a68a0b37a16cd294ccf03c7ee60edf43ae41
InKyoJeong <viapolar@naver.com>
2024-02-28 21:32:23 +0900

    feat(8-8): apply splash screen(android)

--- front/android/app/src/main/java/com/matzipapp/MainActivity.java
+++ front/android/app/src/main/java/com/matzipapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -33,6 +34,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
     super.onCreate(null);
   }
 }
