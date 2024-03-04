2cb881038682662f83fa3240e7b789da3af4568a
InKyoJeong <viapolar@naver.com>
2024-03-05 00:07:38 +0900

    feat(10-1): add post entity

---
+++ server/src/post/post.entity.ts
@@ -0,0 +1,62 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import { MarkerColor } from './marker-color.enum';
+import { ColumnNumericTransformer } from 'src/@common/transformers/numeric.transformer';
+
+@Entity()
+export class Post extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'decimal',
+    transformer: new ColumnNumericTransformer(),
+  })
+  latitude: number;
+
+  @Column({
+    type: 'decimal',
+    transformer: new ColumnNumericTransformer(),
+  })
+  longitude: number;
+
+  @Column()
+  color: MarkerColor;
+
+  @Column()
+  address: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column({
+    type: 'timestamp with time zone',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  date: Date;
+
+  @Column()
+  score: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+}
