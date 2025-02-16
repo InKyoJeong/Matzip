519495cd93b9148934bb09a1d3f69e7ff0cd57f0
InKyoJeong <viapolar@naver.com>
2025-02-16 21:30:57 +0900

    feat(lecture/v77/3-1): add auth home

--- v77/src/screens/auth/AuthHomeScreen.tsx
+++ v77/src/screens/auth/AuthHomeScreen.tsx
@@ -1,7 +1,13 @@
-import {useNavigation} from '@react-navigation/native';
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {authNaviagtions} from '../../constants/navigations';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
 import CustomButton from '../../components/CustomButton';
@@ -13,8 +19,15 @@ type AuthHomeScreenProps = StackScreenProps<
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../../assets/matzip.png')}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.buttonContainer}>
         <CustomButton
           label="로그인하기"
           onPress={() => navigation.navigate(authNaviagtions.LOGIN)}
@@ -29,6 +42,24 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 30,
+    marginVertical: 30,
+  },
+  imageContainer: {
+    flex: 1.5,
+    alignItems: 'center',
+  },
+  image: {
+    width: Dimensions.get('screen').width / 2,
+    height: '100%',
+  },
+  buttonContainer: {
+    flex: 1,
+    gap: 10,
+  },
+});
 
 export default AuthHomeScreen;
