b164715eec3e2eb78352be12f23afbb6686f0a84
InKyoJeong <viapolar@naver.com>
2025-07-06 23:53:48 +0900

    feat(2-1): add screenOptions

--- matzip/src/navigations/AuthNavigation.tsx
+++ matzip/src/navigations/AuthNavigation.tsx
@@ -5,10 +5,40 @@ import LoginScreen from '../screens/auth/LoginScreen';
 import SignupScreen from '../screens/auth/SignupScreen';
 
 const AuthStack = createStackNavigator({
+  screenOptions: {
+    headerTitleAlign: 'center',
+    headerBackButtonDisplayMode: 'minimal',
+    headerTintColor: 'black',
+    headerStyle: {
+      backgroundColor: 'white',
+      shadowColor: 'gray',
+    },
+    headerTitleStyle: {
+      fontSize: 16,
+    },
+    cardStyle: {
+      backgroundColor: 'white',
+    },
+  },
   screens: {
-    AuthHome: AuthHomeScreen,
-    Login: LoginScreen,
-    Signup: SignupScreen,
+    AuthHome: {
+      screen: AuthHomeScreen,
+      options: {
+        headerShown: false,
+      },
+    },
+    Login: {
+      screen: LoginScreen,
+      options: {
+        title: '로그인',
+      },
+    },
+    Signup: {
+      screen: SignupScreen,
+      options: {
+        title: '회원가입',
+      },
+    },
   },
 });
 
--- matzip/src/screens/auth/AuthHomeScreen.tsx
+++ matzip/src/screens/auth/AuthHomeScreen.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 function AuthHomeScreen() {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView>
-      <Text>AuthHomeScreen</Text>
+      <Text onPress={() => navigation.navigate('Login')}>로그인으로 이동</Text>
     </SafeAreaView>
   );
 }
