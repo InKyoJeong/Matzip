8ed604593873f5145aa78c23745453ac615b642d
InKyoJeong <viapolar@naver.com>
2025-07-08 20:47:35 +0900

    feat(3-1): add authHome markup

--- matzip/src/screens/auth/AuthHomeScreen.tsx
+++ matzip/src/screens/auth/AuthHomeScreen.tsx
@@ -1,24 +1,68 @@
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
+import {useNavigation} from '@react-navigation/native';
 
 import {AuthStackParamList} from '@/types/navigation';
 import CustomButton from '@/components/CustomButton';
-import {useNavigation} from '@react-navigation/native';
+import {colors} from '@/constants/colors';
 
 type Navigation = StackNavigationProp<AuthStackParamList>;
 
 function AuthHomeScreen() {
   const navigation = useNavigation<Navigation>();
   return (
-    <SafeAreaView>
-      <CustomButton
-        label="이메일 로그인"
-        onPress={() => navigation.navigate('Login')}
-      />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require('@/assets/matzip.png')}
+          style={styles.image}
+          resizeMode="contain"
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <CustomButton
+          label="이메일 로그인"
+          onPress={() => navigation.navigate('Login')}
+        />
+        <Pressable onPress={() => navigation.navigate('Signup')}>
+          <Text style={styles.emailText}>이메일로 가입하기</Text>
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageContainer: {
+    flex: 1.5,
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: '100%',
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 30,
+    gap: 5,
+  },
+  emailText: {
+    textDecorationLine: 'underline',
+    fontWeight: '500',
+    padding: 10,
+    color: colors.BLACK,
+  },
+});
 
 export default AuthHomeScreen;
