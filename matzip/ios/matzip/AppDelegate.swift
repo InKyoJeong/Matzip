1b1d17f3b6ec8d7378f5ac909b13c7ad60f28ee0
InKyoJeong <viapolar@naver.com>
2025-07-19 22:06:44 +0900

    chore(4-0): install rn maps

--- matzip/ios/matzip/AppDelegate.swift
+++ matzip/ios/matzip/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    GMSServices.provideAPIKey("YOUR_KEY")
+
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
--- matzip/src/screens/map/MapHomeScreen.tsx
+++ matzip/src/screens/map/MapHomeScreen.tsx
@@ -2,20 +2,16 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 import DrawerButton from '@/components/DrawerButton';
-import useAuth from '@/hooks/queries/useAuth';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 function MapHomeScreen() {
-  const {logoutMutation} = useAuth();
-
-  return (
-    <SafeAreaView>
-      <Text>MapHomeScreen</Text>
-      <DrawerButton />
-      <Text onPress={() => logoutMutation.mutate(null)}>로그아웃</Text>
-    </SafeAreaView>
-  );
+  return <MapView style={styles.container} provider={PROVIDER_GOOGLE} />;
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default MapHomeScreen;
