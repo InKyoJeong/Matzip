bb5f05a2c3c074db2455d89a14258c6fdf830ffe
InKyoJeong <viapolar@naver.com>
2025-08-02 19:47:08 +0900

    feat(5-12): add setQueryData

--- matzip/src/hooks/queries/useMutateCreatePost.ts
+++ matzip/src/hooks/queries/useMutateCreatePost.ts
@@ -4,14 +4,31 @@ import {createPost} from '@/api/post';
 import {UseMutationCustomOptions} from '@/types/api';
 import queryClient from '@/api/queryClient';
 import {queryKeys} from '@/constants/keys';
+import {Marker} from '@/types/domain';
 
 function useMutateCreatePost(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: createPost,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
-      });
+    onSuccess: newPost => {
+      // queryClient.invalidateQueries({
+      //   queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+      // });
+      queryClient.setQueryData<Marker[]>(
+        [queryKeys.MARKER, queryKeys.GET_MARKERS],
+        existingMarkers => {
+          const newMarker = {
+            id: newPost.id,
+            latitude: newPost.latitude,
+            longitude: newPost.longitude,
+            color: newPost.color,
+            score: newPost.score,
+          };
+
+          return existingMarkers
+            ? [...existingMarkers, newMarker]
+            : [newMarker];
+        },
+      );
     },
     ...mutationOptions,
   });
