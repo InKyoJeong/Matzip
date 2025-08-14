a735b40ad8df9b1f83281269cdee17b40e85b414
InKyoJeong <viapolar@naver.com>
2025-08-14 22:22:36 +0900

    refactor(7-0)

--- matzip/src/api/axios.ts
+++ matzip/src/api/axios.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import {Platform} from 'react-native';
 
 export const baseUrls = {
-  android: 'http://192.168.219.124:3030',
-  ios: 'http://192.168.219.124:3030',
+  android: 'http://192.168.219.163:3030', // 자신의 주소에 맞게 변경해주세요.
+  ios: 'http://192.168.219.163:3030', // 자신의 주소에 맞게 변경해주세요.
 };
 
 const axiosInstance = axios.create({
--- matzip/src/hooks/queries/useMutateCreatePost.ts
+++ matzip/src/hooks/queries/useMutateCreatePost.ts
@@ -10,25 +10,28 @@ function useMutateCreatePost(mutationOptions?: UseMutationCustomOptions) {
   return useMutation({
     mutationFn: createPost,
     onSuccess: newPost => {
-      // queryClient.invalidateQueries({
-      //   queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
-      // });
-      queryClient.setQueryData<Marker[]>(
-        [queryKeys.MARKER, queryKeys.GET_MARKERS],
-        existingMarkers => {
-          const newMarker = {
-            id: newPost.id,
-            latitude: newPost.latitude,
-            longitude: newPost.longitude,
-            color: newPost.color,
-            score: newPost.score,
-          };
+      queryClient.invalidateQueries({
+        queryKey: [queryKeys.POST, queryKeys.GET_POSTS],
+      }),
+        // queryClient.invalidateQueries({
+        //   queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+        // });
+        queryClient.setQueryData<Marker[]>(
+          [queryKeys.MARKER, queryKeys.GET_MARKERS],
+          existingMarkers => {
+            const newMarker = {
+              id: newPost.id,
+              latitude: newPost.latitude,
+              longitude: newPost.longitude,
+              color: newPost.color,
+              score: newPost.score,
+            };
 
-          return existingMarkers
-            ? [...existingMarkers, newMarker]
-            : [newMarker];
-        },
-      );
+            return existingMarkers
+              ? [...existingMarkers, newMarker]
+              : [newMarker];
+          },
+        );
     },
     ...mutationOptions,
   });
